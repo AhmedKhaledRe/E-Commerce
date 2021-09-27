cd812b65caf92cd928e4ba34e9bf7b2ea69beb9f
Ahmed Khalid <ahmed11khaled93@gmail.com>
2021-09-27 18:00:22 +0200

    modify http to https

--- src/app/services/services.js
+++ src/app/services/services.js
@@ -1,8 +1,8 @@
 import Http from '../../config/http'
 
 const Services = {
-  fetchCategories:async () => await Http.GET(`http://test-api.edfa3ly.io/category`),
-  fetchProducts:async () => await Http.GET(`http://test-api.edfa3ly.io/product`),
+  fetchCategories:async () => await Http.GET(`https://test-api.edfa3ly.io/category`),
+  fetchProducts:async () => await Http.GET(`https://test-api.edfa3ly.io/product`),
 };
 
 export default Services;
